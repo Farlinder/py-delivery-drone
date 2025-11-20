14efcf5bb0e6c07a6d8aad7b66929e68eb6af998
Farlinder <sashakost2006e@gmail.com>
2025-11-20 19:36:19 +0100

    Commit

--- app/main.py
+++ app/main.py
@@ -2,4 +2,7 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
+
+    def __str__(self) -> str:
+        return f"Cargo {self.weight}"
 # write your code here
